c7fe84255a36ad392fcb200579d7d72b4135c8ba
cdnwell <cdnwellhk@gmail.com>
2023-05-17 22:21:55 +0900

    commit : Tetris c# project first init

---
+++ Tetris/Tetris/Diagram.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Tetris
+{
+    internal class Diagram
+    {
+        internal int X
+        {
+            get; private set;
+        }
+        internal int Y
+        {
+            get; private set;
+        }
+        internal Diagram()
+        {
+            Reset();
+        }
+
+        private void Reset()
+        {
+            X = GameRule.SX; 
+            Y = GameRule.SY;
+        }
+        internal void MoveLeft()
+        {
+            X--;
+        }
+        internal void MoveRight() {
+            X++;
+        }
+
+        internal void MoveDown()
+        {
+            Y++;
+        }
+
+    }
+}
